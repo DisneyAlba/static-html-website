667623a1bfafa72652a7646061711dd1328aeccd
DisneyAlba <caledonian.girl@gmail.com>
2021-05-15 18:54:44 +0200

    Toevoegen collapsible content

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,23 @@
-const listItems = document.querySelectorAll("li");
+function makeContentAppear(teller) {
+    const collapsibleContent = document.getElementById("CollapsibleContent" + teller);
+    collapsibleContent.classList.toggle("open");
+}
 
-console.log(`We have ${listItems.length} list items on this page`);
+
+/*
+Oplossing van Codaisseur:
+
+Geen onclick attribute in de button, maar button wel class collapsible geven (in mijn CSS: selector button)
+Geen ID in the div
+
+const collapsibles = document.querySelectorAll(".collapsible");
+
+for (let i = 0; i < collapsibles.length; i++) {
+  const collapsible = collapsibles[i];
+
+  collapsible.addEventListener("click", function () {
+    const collapsibleContent = collapsible.nextElementSibling;
+    collapsibleContent.classList.toggle("visible");
+  });
+}
+*/
